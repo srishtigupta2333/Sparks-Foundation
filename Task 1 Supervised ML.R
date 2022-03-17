7505972f9f52629d13b8cc52a5651b04999b053e
srishtigupta2333 <srishtigupta2333@gmail.com>
2022-03-17 17:27:24 +0530

    final commit 17-3-22

--- Task 1 Supervised ML.R
+++ Task 1 Supervised ML.R
@@ -6,23 +6,50 @@
 ## Aim- to predict the percentage of a student based on the no. of study hours ##
 ## Question- What will be the predicted score if a student studies for 9.25 hrs/day ##
 
-
 ## Importing Data into R ##
 library(readr)
-data <- read_csv("student_scores - student_scores.csv")
-View(data)
+student_scores <- read_csv("student_scores - student_scores.csv")
+View(student_scores)
 
 ## installing ggplot2 package to create a scatter plot for the data ##
 install.packages("ggplot2")
 library(ggplot2)
-ggplot(data, aes(x = Hours,
-                 y = Scores, 
-                 color = I("Blue"))) +
-        geom_point()
+ggplot(student_scores, aes(x = Hours,
+                           y = Scores, 
+                           color = I("Blue"))) +
+  geom_point()
 
 ## using lm function for regression, to generate slope and intercept for the Data
-linear_model <- lm(Scores ~ Hours, data = data)
+linear_model <- lm(Scores ~ Hours, data = student_scores)
 summary(linear_model)
+
+## we found our slope = 9.7758 and our intercept = 2.4387 ##
+## We can add a linear regression line in our scatter plot ##
+
+ggplot(student_scores, aes(x= Hours,
+                           y = Scores,
+                           color =I("Blue"))) +
+  geom_point() +
+  geom_abline(aes(intercept = 2.4387, slope = 9.7758, color = I("Red")))
+
+## Add prediction values in data ##
+student_scores$Predicted <- linear_model$fitted.values
+
+## Predict scores if a student studies for 9.25 hours a day ##
+predict(linear_model, data.frame(Hours = 9.25))
+
+## Plotting a bar plot of actual vs predicted scores ##
+barplot(student_scores$Scores, col = "blue")
+barplot(student_scores$Predicted, col = "red")
+
+## we can verify this predicted value by equation of line i.e.,##
+## y = intercept + x*slope ##
+linear_model$coefficients
+y = 2.483673 + 9.25*9.775803
+y
+
+## Thus as seen from the model,##
+## if a student studies 9.25hrs/day then he/she will get approximately 93 marks ##
 
 
 
